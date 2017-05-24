7e255137580975086acfbde50db4f48540ba55b4
formailmagazineaddr <formailmagazineaddr@yahoo.co.jp>
2017-05-25 01:40:56 +0900

    initial commit

--- app/src/main/java/com/example/user/locationmaptest/MapsActivity.java
+++ app/src/main/java/com/example/user/locationmaptest/MapsActivity.java
@@ -1,181 +1,91 @@
-package com.example.user.maplocationtest;
+package com.example.user.fusedlocationtest;
 
-import android.support.v4.app.ActivityCompat;
-import android.Manifest;
-import android.content.pm.PackageManager;
+//package com.example.user.maptest;
+import android.content.Intent;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        LocationListener, GoogleMap.OnMyLocationButtonClickListener, LocationSource {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest locationRequest;
+    private LatLng iamhere;
+    private float zoomLevel = 10.0F;
 
-    private OnLocationChangedListener onLocationChangedListener = null;
-
-    private int priority[] = {LocationRequest.PRIORITY_HIGH_ACCURACY, LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY,
-            LocationRequest.PRIORITY_LOW_POWER, LocationRequest.PRIORITY_NO_POWER};
-    private int locationPriority;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        // LocationRequest を生成して精度、インターバルを設定
-        locationRequest = LocationRequest.create();
-
-        // 測位の精度、消費電力の優先度
-        locationPriority = priority[1];
-
-        if(locationPriority == priority[0]){
-            // 位置情報の精度を優先する場合
-            locationRequest.setPriority(locationPriority);
-            locationRequest.setInterval(5000);
-            locationRequest.setFastestInterval(16);
-        }
-        else if(locationPriority == priority[1]){
-            // 消費電力を考慮する場合
-            locationRequest.setPriority(locationPriority);
-            locationRequest.setInterval(60000);
-            locationRequest.setFastestInterval(16);
-        }
-        else if(locationPriority == priority[2]){
-            // "city" level accuracy
-            locationRequest.setPriority(locationPriority);
-        }
-        else{
-            // 外部からのトリガーでの測位のみ
-            locationRequest.setPriority(locationPriority);
-        }
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
     }
 
-    // onResumeフェーズに入ったら接続
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mGoogleApiClient.connect();
-    }
 
-    // onPauseで切断
-    @Override
-    public void onPause() {
-        super.onPause();
-        mGoogleApiClient.disconnect();
-    }
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-        // check permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Log.d("debug", "permission granted");
+        //get the lat and lon from the previous activity
+        Intent intent = getIntent();
+        double latitude = intent.getDoubleExtra("currentLat", 0);
+        double longitude = intent.getDoubleExtra("currentLon", 0);
 
-            mMap = googleMap;
-            // default の LocationSource から自前のsourceに変更する
-            mMap.setLocationSource(this);
-            mMap.setMyLocationEnabled(true);
-            mMap.setOnMyLocationButtonClickListener(this);
-        }
-        else{
-            Log.d("debug", "permission error");
-            return;
-        }
+        //add marker
+        iamhere = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(iamhere).title("I am here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iamhere, zoomLevel));
+
+        // タップした時のリスナーをセット
+        mMap.setOnMapClickListener(new OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng tapLocation) {
+                // tapされた位置の緯度経度
+                iamhere = new LatLng(tapLocation.latitude, tapLocation.longitude);
+                mMap.addMarker(new MarkerOptions().position(iamhere).title(""+tapLocation.latitude+" :"+ tapLocation.longitude));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iamhere, zoomLevel));
+
+            }
+        });
+
+        // 長押しのリスナーをセット
+        mMap.setOnMapLongClickListener(new OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng longpushLocation) {
+                LatLng newlocation = new LatLng(longpushLocation.latitude, longpushLocation.longitude);
+                mMap.addMarker(new MarkerOptions().position(newlocation).title(""+longpushLocation.latitude+" :"+ longpushLocation.longitude));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newlocation, zoomLevel));
+            }
+        });
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d("debug","onLocationChanged");
-        if (onLocationChangedListener != null) {
-            onLocationChangedListener.onLocationChanged(location);
-
-            double lat = location.getLatitude();
-            double lng = location.getLongitude();
-
-            Log.d("debug","location="+lat+","+lng);
-
-            Toast.makeText(this, "location="+lat+","+lng, Toast.LENGTH_SHORT).show();
-
-            // Add a marker and move the camera
-            LatLng newLocation = new LatLng(lat, lng);
-            mMap.addMarker(new MarkerOptions().position(newLocation).title("My Location"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
-
-        }
+    //-------------------------------------
+    // called when pushed 'delegate' button
+    //-------------------------------------
+    public void delegateButtonHandler(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
-        // check permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Log.d("debug", "permission granted");
-
-            // FusedLocationApi
-            LocationServices.FusedLocationApi.requestLocationUpdates(
-                    mGoogleApiClient, locationRequest, this);
-        }
-        else{
-            Log.d("debug", "permission error");
-            return;
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d("debug", "onConnectionSuspended");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.d("debug", "onConnectionFailed");
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "onMyLocationButtonClick", Toast.LENGTH_SHORT).show();
-
-        return false;
-    }
-
-    // OnLocationChangedListener calls activate() method
-    @Override
-    public void activate(OnLocationChangedListener onLocationChangedListener) {
-        this.onLocationChangedListener = onLocationChangedListener;
-    }
-
-    @Override
-    public void deactivate() {
-        this.onLocationChangedListener = null;
-    }
 }
